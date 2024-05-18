34ccc29a7708a3311717e71d9c6be7aa6424877c
Daley Mottley <daley.mottley@hotmail.com>
2024-05-18 17:19:21 +0000

    Change browser to firefox

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from seleniumbase import SB
 
-with SB(uc=True, browser='chrome', headless=False, headed=True) as sb:
+with SB(uc=True, browser='firefox', headless=False, headed=True) as sb:
     sb.driver.get("https://www.yahoo.com/")
