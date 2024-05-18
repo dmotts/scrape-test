fee99c78b5de7a5514ec5010da41513ba56b3457
Daley Mottley <daley.mottley@hotmail.com>
2024-05-18 01:42:46 +0000

    Change methods

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from seleniumbase import Driver
 driver = Driver(uc=True, browser='chrome', headless=False)
 url = "https://gitlab.com/users/sign_in"
 try: 
-    driver.uc_open_with_reconnect(url, 3)
-    driver.sleep(10)
+    driver.uc_open(url)
+    print(f"Opened {url}")
+    driver.sleep(1000)
 finally:
     driver.quit()
