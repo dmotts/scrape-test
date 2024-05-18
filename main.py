970967a4f4570fbaf7a5f59cf1412ff7fb4587be
Daley Mottley <daley.mottley@hotmail.com>
2024-05-18 01:27:50 +0000

    Refactor code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from seleniumbase import Driver
 
-driver = Driver(uc=True)
+driver = Driver(uc=True, browser='chrome', headless=False)
 url = "https://gitlab.com/users/sign_in"
-driver.uc_open_with_reconnect(url, 3)
-driver.sleep(10)
-driver.quit()
+try: 
+    driver.uc_open_with_reconnect(url, 3)
+    driver.sleep(10)
+finally:
+    driver.quit()
